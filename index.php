d147a09ff1fccddfda731f03b53a03b7def8a172
shasmsularefinrabby <rabbyshamsularefin@gmail.com>
2023-10-04 02:49:39 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,91 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700&display=swap">
+  <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./admin/lib/font-awesome/css/font-awesome.css">
+  <link rel="stylesheet" href="./css/style.css">
+
+  <title></title>
+</head>
+
+<body>
+
+  <!-- Loader -->
+  <!-- <div class="loader">
+    <div class="spinner">
+      <div class="double-bounce1"></div>
+      <div class="double-bounce2"></div>
+    </div> 
+  </div> -->
+
+
+
+
+
+
+
+
+
+
+  <!-- Click capture -->
+  <div class="click-capture d-lg-none"></div>
+
+  <!-- Navbar -->
+  <nav id="scrollspy" class="navbar navbar-desctop">
+
+    <div class="d-flex mr-0 position-relative w-100">
+
+      <!-- Brand-->
+      <a class="navbar-brand" href="#">LOGO</a>
+      <ul class="navbar-nav-desctop ml-auto d-none d-lg-block">
+        <li><a class="nav-link" href="#home">Home</a></li>
+        <li><a class="nav-link" href="#about">About</a></li>
+        <li><a class="nav-link" href="#experience">Experience</a></li>
+        <li><a class="nav-link" href="#projects">Projects</a></li>
+        <li><a class="nav-link" href="#testimonials">Testimonials</a></li>
+      </ul>
+
+      <!-- Social -->
+     
+
+      <!-- Toggler -->
+      <button class="toggler d-lg-none ml-auto">
+        <span class="toggler-icon"></span>
+        <span class="toggler-icon"></span>
+        <span class="toggler-icon"></span>
+      </button>
+    </div>
+  </nav>
+
+
+  <!-- Navbar Mobile -->
+  <nav id="navbar-mobile" class="navbar navbar-mobile d-lg-none">
+    <ion-icon class="close" name="close-outline"></ion-icon>
+
+    <!-- Social -->
+    <ul class="social-icons mr-auto mr-lg-0">
+      <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
+      <li><a href="#"><ion-icon name="logo-twitter"></ion-icon></a></li>
+      <li><a href="#"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+      <li><a href="#"> <ion-icon name="logo-instagram"></ion-icon></a></li>
+    </ul>
+
+    <ul class="navbar-nav navbar-nav-mobile">
+      <li><a class="nav-link" href="#home">Home</a></li>
+      <li><a class="nav-link" href="#about">About</a></li>
+      <li><a class="nav-link" href="#experience">Experience</a></li>
+      <li><a class="nav-link" href="#projects">Projects</a></li>
+      <li><a class="nav-link" href="#testimonials">Testimonials</a></li>
+    </ul>
+    <div class="navbar-mobile-footer">
+      <p>© 2020 Richard. All Rights Reserved.</p>
+    </div>
+  </nav>
+
+ 
