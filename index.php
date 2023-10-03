c6af3fac76ada5c73c0d2ac54b660183c5d5e9ab
shasmsularefinrabby <rabbyshamsularefin@gmail.com>
2023-10-04 04:16:10 +0600

    slider add

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="./admin/lib/font-awesome/css/font-awesome.css">
   <link rel="stylesheet" href="./css/style.css">
+  <link rel="stylesheet" href="./slider/css/style.css">
 
   <title></title>
 </head>
@@ -42,14 +43,18 @@
     <div class="d-flex mr-0 position-relative w-100">
 
       <!-- Brand-->
-      <a class="navbar-brand" href="#">LOGO</a>
-      <ul class="navbar-nav-desctop ml-auto d-none d-lg-block">
+      <a class="navbar-brand mr-auto mb-auto mt-auto" href="#">LOGO</a>
+      <ul class="navbar-nav-desctop  m-auto d-none d-lg-block">
         <li><a class="nav-link" href="#home">Home</a></li>
         <li><a class="nav-link" href="#about">About</a></li>
-        <li><a class="nav-link" href="#experience">Experience</a></li>
-        <li><a class="nav-link" href="#projects">Projects</a></li>
-        <li><a class="nav-link" href="#testimonials">Testimonials</a></li>
+        <li><a class="nav-link" href="#experience">Profiles</a></li>
+        <li><a class="nav-link" href="#projects">FaQ's</a></li>
+        <li><a class="nav-link" href="#testimonials">Vlog</a></li>
       </ul>
+      <a href="">
+        <button type="button" class="btn btn-info">Login</button>
+        <button type="button" class="btn btn-info">Register</button>
+      </a>
 
       <!-- Social -->
      
@@ -87,5 +92,33 @@
       <p>© 2020 Richard. All Rights Reserved.</p>
     </div>
   </nav>
-
+  
+  <section class="section">
+  <div class="container">
+        <div class="slider">
+        <div class="slide-viewer">
+          <div class="slide-group">
+            <div class="slide slide-1">
+              <img src="./slider/img/slide-1.jpg" alt="slide_01" />
+            </div>
+            <div class="slide slide-2">
+              <img src="./slider/img/slide-2.jpg" alt="slide_02"  />
+            </div>
+            <div class="slide slide-3">
+              <img src="./slider/img/slide-3.jpg" alt="slide_03"  />
+            </div>
+            <div class="slide slide-4">
+              <img src="./slider/img/slide-4.jpg" alt="slide_04"  />
+            </div>
+          </div>
+        </div>
+        <div class="slide-buttons"></div>
+      </div>
+    </div>
+  </section>
  
+
+
+
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script> 
+   <script src="./slider/js/index.js"></script>
