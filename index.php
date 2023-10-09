c053329b4883f16138eea570bdc7ca68b48cee90
shasmsularefinrabby <rabbyshamsularefin@gmail.com>
2023-10-10 02:47:45 +0600

    add video

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
-
 <!doctype html>
-<html lang="en">
+
 
 <head>
-  
+
 
   <!-- CSS -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700&display=swap">
   <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="./admin/lib/font-awesome/css/font-awesome.css">
   <link rel="stylesheet" href="./css/style.css">
+  <link rel="stylesheet" href="./slider/css/styles.css">
+
+
+
+
 
   <title></title>
 </head>
@@ -42,17 +46,21 @@
     <div class="d-flex mr-0 position-relative w-100">
 
       <!-- Brand-->
-      <a class="navbar-brand" href="#">LOGO</a>
-      <ul class="navbar-nav-desctop ml-auto d-none d-lg-block">
+      <a class="navbar-brand mr-auto mb-auto mt-auto" href="#">LOGO</a>
+      <ul class="navbar-nav-desctop  m-auto d-none d-lg-block">
         <li><a class="nav-link" href="#home">Home</a></li>
         <li><a class="nav-link" href="#about">About</a></li>
-        <li><a class="nav-link" href="#experience">Experience</a></li>
-        <li><a class="nav-link" href="#projects">Projects</a></li>
-        <li><a class="nav-link" href="#testimonials">Testimonials</a></li>
+        <li><a class="nav-link" href="#experience">Profiles</a></li>
+        <li><a class="nav-link" href="#projects">FaQ's</a></li>
+        <li><a class="nav-link" href="#testimonials">Vlog</a></li>
       </ul>
+      <a href="">
+        <button type="button" class="btn btn-info">Login</button>
+        <button type="button" class="btn btn-info">Register</button>
+      </a>
 
       <!-- Social -->
-     
+
 
       <!-- Toggler -->
       <button class="toggler d-lg-none ml-auto">
@@ -69,12 +77,7 @@
     <ion-icon class="close" name="close-outline"></ion-icon>
 
     <!-- Social -->
-    <ul class="social-icons mr-auto mr-lg-0">
-      <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
-      <li><a href="#"><ion-icon name="logo-twitter"></ion-icon></a></li>
-      <li><a href="#"><ion-icon name="logo-linkedin"></ion-icon></a></li>
-      <li><a href="#"> <ion-icon name="logo-instagram"></ion-icon></a></li>
-    </ul>
+   
 
     <ul class="navbar-nav navbar-nav-mobile">
       <li><a class="nav-link" href="#home">Home</a></li>
@@ -83,9 +86,198 @@
       <li><a class="nav-link" href="#projects">Projects</a></li>
       <li><a class="nav-link" href="#testimonials">Testimonials</a></li>
     </ul>
-    <div class="navbar-mobile-footer">
-      <p>© 2020 Richard. All Rights Reserved.</p>
-    </div>
+    
   </nav>
 
- 
+  <!-- slider start -->
+
+  <section>
+        <div class="container container-slider">
+            <div class="row">
+                <div id="slider">
+                    <div class="slide">
+                        <img src="./slider/Img/slider-1.jpg" alt="Image 1">
+                    </div>
+                    <div class="slide">
+                        <img src="./slider/Img/slider-2.jpg" alt="Image 2">
+                    </div>
+                    <div class="slide">
+                        <img src="./slider/Img/slider-3.jpg" alt="Image 3">
+                    </div>
+                    <div class="slide">
+                        <img src="./slider/Img/slider-4.jpg" alt="Image 4">
+                    </div>
+                    <div class="slide">
+                        <img src="./slider/Img/slider-5.jpg" alt="Image 5">
+                    </div>
+                    <div class="slide">
+                        <img src="./slider/Img/slider-6.jpg" alt="Image 6">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+  <!-- slider End -->
+
+  <!-- service Start -->
+
+  <section class="section">
+    <div class="container">
+      <div class="row align-items-end">
+        <div class="col-md-6" data-aos="fade-up">
+          <h2 class="mb-2 mb-md-0">Services</h2>
+        </div>
+      </div>
+      <div class="mt-3 pt-3" data-aos="fade-in">
+        <div class="carousel-project owl-carousel">
+          <div class="service-item">
+            <picture class="position-relative">
+              <img alt="" class="w-100" src="./slider/Img/slider-6.jpg">
+              <div class="text-black">
+                <h3 class="mb-2 ">Front-End Development</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, pariatur.</p>
+              </div>
+            </picture>
+          </div>
+          <div class="service-item">
+            <picture class="position-relative">
+              <img alt="" class="w-100" src="./slider/Img/slider-2.jpg">
+              <div class="text-black">
+                <h3 class="mb-2">Back-End Development</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium iste ullam molestias at debitis quae accusantium, temporibus porro vitae. Tempora.</p>
+              </div>
+            </picture>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- service End -->
+
+  <!-- FAQ and video part start -->
+  
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="d-flex flex-column mb-3">
+            <div class="bd-highlight">
+              <h3>FAQ's</h3>
+            </div>
+            <div class="p-2 bd-highlight">
+              <ul>
+                <li><a href="#">What is modeling?</a></li>
+                <li><a href="#">How to hier a model for your Show?</a></li>
+
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="d-flex flex-row mb-3">
+            <div class="p-2 bd-highlight">
+              <video width="640" height="360" controls>
+                <source src="./videos/দুর্ধর্ষ ড্রোন দিয়ে ইসরাইলে হামাসের বাজিমাত _ Israel-Palestine War _ Hamas _ Somoy TV.mp4" type="video/mp4">
+              </video>
+              <div>
+                <h2>Contact Us</h2>
+                <h5>115,Lake circus kalabagan Dhaka-1205</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<!-- FAQ and video part end -->
+
+<!-- vlogs part start -->
+
+<section class="section">
+    <div class="container">
+      <div class="row align-items-end">
+        <div class="col-md-6" data-aos="fade-up">
+          <h2 class="mb-2 mb-md-0">Vlogs</h2>
+        </div>
+      </div>
+      <div class="mt-3 pt-3" data-aos="fade-in">
+        <div class="carousel-project owl-carousel">
+          <div class="service-item">
+            <picture class="position-relative">
+              <img alt="" class="w-100" src="./slider/Img/travlling vlog.webp">
+              <div class="text-black">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam soluta unde fuga perspiciatis impedit, in libero, architecto pariatur harum expedita tempore veritatis consectetur tempora inventore velit hic, sapiente sequi minima? Laboriosam aliquam, temporibus id molestias officiis sed eos tempore repellat iure necessitatibus totam praesentium sit ea voluptas cumque eius quo minus, delectus deserunt veniam nihil? Illum id nemo natus enim nam neque animi deserunt, omnis, consequuntur explicabo placeat obcaecati eius amet pariatur rem sint quam quidem soluta repudiandae excepturi non quod et sunt? Nisi, obcaecati! Aliquam reprehenderit autem nulla dolorem tenetur dignissimos perferendis tempore saepe perspiciatis eum, recusandae itaque laudantium accusantium vero voluptates error quos quis. Amet reprehenderit, nisi vitae quae quos cumque hic aperiam aspernatur modi magnam laudantium. Neque magni ducimus eius dicta porro exercitationem laudantium maxime distinctio vitae nemo! Iure, recusandae illo? Quas magni, illum quibusdam exercitationem aliquid expedita deserunt, recusandae sequi sed, quis velit neque. Ratione assumenda totam ea, repudiandae deserunt at possimus incidunt similique fugit commodi saepe perferendis illo labore et ipsa iste iure consequuntur eius? Perferendis dicta iusto, voluptatem sit rem voluptas deleniti sapiente dolore eum quasi! Minima culpa autem veniam, ratione officia nostrum perferendis fugit molestias temporibus sapiente quasi corporis adipisci harum deserunt ex. Animi accusantium tempore, at voluptas adipisci temporibus possimus dolore est voluptatibus. Suscipit quos corrupti labore reprehenderit odit explicabo officia quae magni ullam accusamus debitis velit consequatur inventore, quasi dicta, tempore maxime autem similique amet consectetur molestias dolorum. Commodi incidunt possimus culpa nostrum placeat ducimus laboriosam harum, beatae deserunt deleniti officia fugiat tempora eligendi qui inventore minima labore illum minus? Nihil sit laborum facilis. Nulla quibusdam aperiam fugit mollitia id facilis labore sequi ex facere. Dolor, voluptatem necessitatibus placeat facere numquam aspernatur esse possimus odio eos tenetur magni natus aliquid velit voluptates ex! Aliquam ex commodi delectus vero a inventore autem corrupti in libero quod hic quam distinctio debitis, culpa modi nemo provident neque mollitia earum placeat recusandae necessitatibus. Praesentium ullam dignissimos laboriosam cumque sit, aut cum suscipit dolor eos maiores quaerat accusantium quam reiciendis consequatur ducimus autem, sequi, provident unde! Non perferendis id placeat recusandae illum deleniti incidunt aliquid. Praesentium repellendus, in non, delectus quod tempore fuga sint, et modi hic facilis aperiam dicta atque pariatur maiores repellat ea consequatur id ipsam? Sequi, deserunt corrupti exercitationem quis commodi in fuga similique odio quos esse, numquam expedita, atque nemo consequatur fugiat facere vel velit. Accusantium qui, harum adipisci rerum ipsum dolore dolorum ad perspiciatis necessitatibus magni sed quis consectetur placeat fuga natus, ex totam? Magnam eligendi earum, molestias quas laudantium quis possimus temporibus fugit odit dolorem molestiae ullam a recusandae impedit amet unde nobis rerum tempora labore aperiam sunt natus! Cupiditate enim odit nam alias eum ex magnam quidem, itaque placeat optio. Officiis aperiam, iste maiores numquam, nesciunt eligendi perspiciatis vero explicabo dolor molestiae ipsum nobis quod praesentium iure accusamus eveniet earum nemo eaque error quisquam consectetur corrupti molestias obcaecati repellendus? Voluptas tempora placeat magnam ducimus veritatis consectetur, nihil esse libero vel autem assumenda itaque, laborum repellat rerum aspernatur aliquid modi.</p>
+              </div>
+            </picture>
+          </div>
+          <div class="service-item">
+            <picture class="position-relative">
+              <img alt="" class="w-100" src="./slider/Img/slider-2.jpg">
+              <div class="text-black">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus fuga placeat qui quibusdam debitis, amet tenetur inventore suscipit. Ea pariatur magni molestiae, voluptates modi blanditiis accusantium ab quas sit, inventore nesciunt velit ullam, beatae ipsam assumenda quisquam temporibus. Modi laudantium ad hic quo quidem architecto dolorem iure deleniti delectus eius!</p>
+              </div>
+            </picture>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+<!-- vlogs part end -->
+
+<!-- Footer Start -->  
+
+<footer>
+      <div class="section bg-dark py-5 pb-0">
+        <div class="container">
+          <div class="row">
+           <div class="col-md-6 col-lg-4">
+             <h6 class="text-white mb-4">Phone:</h6>
+             <p class="text-white mb-4">+72323156466:</p>
+            </div>
+            <div class="col-md-6 col-lg-4">
+             <h6 class="text-white mb-4">Email:</h6>
+             <p class="text-white mb-4">Richard@example.com</p>
+            </div>
+            <div class="col-md-6 col-lg-4">
+              <h6 class="text-white mb-4">Follow me:</h6>
+              <ul class="social-icons">
+               <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
+               <li><a href="#"><ion-icon name="logo-twitter"></ion-icon></a></li>
+               <li><a href="#"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+               <li><a href="#"> <ion-icon name="logo-instagram"></ion-icon></a></li>
+            </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer-copy section-sm">
+        <div class="container">© Copyright 2020 Richard. All Rights Reserved</div>
+       </div>
+    </footer>
+<!-- Footer End -->  
+
+
+
+
+  <script src="./slider/Js/script.js"></script>
+  <script src="js/jquery-1.12.4.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+  <script src="js/jarallax.min.js"></script>
+  <script src="js/jquery.ajaxchimp.min.js"></script>
+  <script src="js/jquery.validate.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/interface.js"></script>
+
+
+
+</body>
+
+</html>
