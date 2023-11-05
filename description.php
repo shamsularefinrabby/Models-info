788aa44e4b94cbe382d2bb6172e78e03a5fd8b4a
shasmsularefinrabby <rabbyshamsularefin@gmail.com>
2023-11-06 04:28:53 +0600

    description page work

--- description.php
+++ description.php
@@ -34,9 +34,9 @@
       <div class="d-flex mr-0 position-relative w-100">
 
         <!-- Brand-->
-        <a class="navbar-brand mr-auto mb-auto mt-auto" href="#">LOGO</a>
+        <a class="navbar-brand mr-auto mb-auto mt-auto" href="index.php">LOGO</a>
         <ul class="navbar-nav-desctop  m-auto d-none d-lg-block">
-          <li><a class="nav-link" href="#home">Home</a></li>
+          <li><a class="nav-link" href="index.php">Home</a></li>
           <li><a class="nav-link" href="#about">About</a></li>
           <li><a class="nav-link" href="#profile">Profiles</a></li>
           <li><a class="nav-link" href="#faq's">FaQ's</a></li>
@@ -77,7 +77,6 @@
 
   </section>
 
-  card
 
 
   <section class="section">
@@ -90,25 +89,116 @@
             </div>
           </div>
         </div>
-        <div class="col-lg-8">
-          <div class="d-flex flex-column mb-3">
 
-            <form>
-              <div class="col-4">
-                <label class="form-label">Email address</label>
-                <input type="email" class="form-control">
-                
+        <div class="col-7 ">
+          <div class="d-flex flex-column">
+            <form action="#" method="" enctype="">
+              <div class="">
+                <div class="row mg-b-25">
+
+                  <div class="col-6">
+                    <div class="form-group">
+                      <label class="form-control-label">First Name: <span class="tx-danger">*</span></label>
+                      <input class="form-control" type="text" name="">
+                    </div>
+                  </div><!-- col-4 -->
+
+                  <div class="col-6">
+                    <div class="form-group mg-b-10-force">
+                      <label class="form-control-label">Last Name<span class="tx-danger">*</span></label>
+                      <input class="form-control" type="text" name="">
+                    </div>
+                  </div><!-- col-8 -->
+
+                  <div class="form-group col-12">
+                    <label>Email address: <span class="tx-danger">*</span></label>
+                    <input type="email" class="form-control" placeholder="abc@def.com">
+                  </div>
+
+                  <div class="form-group col-12">
+                    <label>Password: <span class="tx-danger">*</span></label>
+                    <input type="Password" class="form-control" placeholder="Enter your password">
+                  </div>
+
+                  <div class="form-group col-12">
+                    <label>Confirm Password: <span class="tx-danger">*</span></label>
+                    <input type="Password" class="form-control" placeholder="Confirm Your Password">
+                  </div>
+
+                  <div class="form-group col-6">
+                    <label>Present Address: <span class="tx-danger">*</span></label>
+                    <input type="text" class="form-control" placeholder="">
+                  </div>
+                  <div class="form-group col-6">
+                    <label>Parmanent Address: <span class="tx-danger">*</span></label>
+                    <input type="text" class="form-control" placeholder="">
+                  </div>
+
+                  <div class="form-group col-4">
+                    <label>Date Of Birth: <span class="tx-danger">*</span></label>
+                    <input type="date" class="form-control" placeholder="">
+                  </div>
+
+                  <div class="form-group col-4">
+                    <label>Nationality: <span class="tx-danger">*</span></label>
+                    <input type="text" class="form-control" placeholder="">
+                  </div>
+
+                  <div class="form-group col-4">
+                    <label>Place Of Birth: <span class="tx-danger">*</span></label>
+                    <input type="text" class="form-control" placeholder="">
+                  </div>
+
+                </div>
               </div>
-              <div class="col-4">
-                <label class="form-label">Password</label>
-                <input type="password" class="form-control" >
-              </div>
-              
             </form>
+          </div>
+        </div>
 
-          <!-- </div> -->
+        <div class="col-2">
+          <div class="d-flex flex-column mb-3 float-end">
+            <div class="form-layout ">
+              <button class="btn  btn-info mg-r-5">Submit</button>
+            </div>
+          </div>
         </div>
       </div>
+    </div>
+  </section>
+
+
+  <section class="section">
+    <div class="container">
+      <div class="col-12">
+        <div class="row">
+          <h3>Images</h3>
+          <div class="card">
+            <div class="d-flex flex-row mb-3 col-4">
+              <div class="card-body d-flex flex-column">
+                <img class="card-img-top img-thumbnail" style="width: 200px; height: 200px;" src="./img/12345.jpg " alt="">
+              </div>
+            </div>
+
+
+
+            <nav aria-label="Page navigation example">
+              <ul class="pagination justify-content-end">
+                <li class="page-item disabled">
+                  <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                  <a class="page-link" href="#">Next</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+        </div>
+      </div>
+
     </div>
   </section>
 
